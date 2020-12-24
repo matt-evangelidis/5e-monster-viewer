ebadb27640f000cbf7a55d72c3a647c22f804ad9
Matthew Evangelidis <matthew.evangelidis@students.mq.edu.au>
2020-12-24 21:17:01 +1100

    Reconsidered and reformatted Creature class out of interfaces
    Interfaces are designed around each major section of the Creature class, and allow for the Props sections of each component to simply declare an object of an interface type rather than re-declaring sections of the Creature class.

--- src/types/Creature.tsx
+++ src/types/Creature.tsx
@@ -12,127 +12,24 @@ class Creature {
         value: number;
         from: string;
     }
-    speeds!: {
-        base: number;
-        climb?: number;
-        fly?: number;
-        swim?: number;
-        burrow?: number;
-        hover?: boolean;
-    }
-    
-    stats!: { //accessible as 0-5
-        strength: number;//0
-        dexterity: number;//1
-        constitution: number;//2
-        intelligence: number;//3/
-        wisdom: number;//4
-        charisma: number;//5
-    }
-    savingThrows?: {
-        str?: boolean;
-        dex?: boolean;
-        con?: boolean;
-        int?: boolean;
-        wis?: boolean;
-        cha?: boolean;
-    }
-    skills?: {//18 total skills
-        //str
-        athletics?: [number, boolean];
-        //dex
-        acrobatics?: [number, boolean];
-        sleightOfHand?: [number, boolean];
-        stealth?: [number, boolean];
-        //int
-        arcana?: [number, boolean];
-        history?: [number, boolean];
-        investigation?: [number, boolean];
-        nature?: [number, boolean];
-        religion?: [number, boolean];
-        //wis
-        animalHandling?: [number, boolean];
-        insight?: [number, boolean];
-        medicine?: [number, boolean];
-        perception?: [number, boolean];
-        survival?: [number, boolean];
-        //cha
-        deception?: [number, boolean];
-        intimidation?: [number, boolean];
-        performance?: [number, boolean];
-        persuasion?: [number, boolean];
-    }
-    damageResistImmune?: {//1 is resist, 2 is immune
-        acid?: number;
-        bludgeoning?: number;
-        cold?: number;
-        fire?: number;
-        force?: number;
-        lightning?: number;
-        necrotic?: number;
-        piercing?: number;
-        poison?: number;
-        psychic?: number;
-        radiant?: number;
-        slashing?: number;
-        thunder?: number;
-    }
-    damageVulnerable?: {
-        acid?: boolean;
-        bludgeoning?: boolean;
-        cold?: boolean;
-        fire?: boolean;
-        force?: boolean;
-        lightning?: boolean;
-        necrotic?: boolean;
-        piercing?: boolean;
-        poison?: boolean;
-        psychic?: boolean;
-        radiant?: boolean;
-        slashing?: boolean;
-        thunder?: boolean;
-    }
-    conditionImmune?: {
-        blinded?: boolean;
-        charmed?: boolean;
-        deafened?: boolean;
-        exhaustion?: boolean;
-        frightened?: boolean;
-        grappled?: boolean;
-        incapacitated?: boolean;
-        invisible?: boolean;
-        paralyzed?: boolean;
-        petrified?: boolean;
-        poisoned?: boolean;
-        prone?: boolean;
-        restrained?: boolean;
-        stunned?: boolean;
-        unconscious?: boolean;
-    }
-    senses!:{
-        blindsight?: number;
-        darkvision?: number;
-        tremorsense?: number;
-        truesight?: number;
-        //passive
-    }
+    speeds!: ISpeeds;
+    stats!: IStats;
+    saves?: ISaves;
+    skills!: ISkills;
+
+    damageResistImmune?: IDamageResistImmune;
+    damageVulnerable?: IDamageVulnerable;
+    conditionImmune?: IConditionImmune;
+    senses!: ISenses;
+
     languages!: string[];
     challenge!: number;
     traits!: entry[];
     actions!: entry[];
-    legendary!: boolean;
+    legendary?: boolean;
+    mythic?: boolean;
     legendaryActions?: entry[];
 
-    //functions
-    
-    // public get speed() : Array<any> {
-    //     let array = [];
-    //     for (let element in this.speeds) {
-    //         array.push(element)
-    //     }
-    //     return array;
-    // }
-    
 };
 
 interface entry {
@@ -140,12 +37,116 @@ interface entry {
     desc: string;
 };
 
-interface ICreature extends Creature{};
+export interface ISpeeds {
+    base: number;
+    climb?: number;
+    fly?: number;
+    swim?: number;
+    burrow?: number;
+    hover?: boolean;
+};
 
-type CreaturePropsArray = Array<keyof ICreature>;
+export interface IStats {
+    strength: number;//0
+    dexterity: number;//1
+    constitution: number;//2
+    intelligence: number;//3/
+    wisdom: number;//4
+    charisma: number;//5
+};
 
-const propsArray: CreaturePropsArray = 
-    Object.keys(new Creature()) as CreaturePropsArray;
-console.log(propsArray);
+export interface ISaves {
+    strength?: boolean;
+    dexterity?: boolean;
+    constitution?: boolean;
+    intelligence?: boolean;
+    wisdom?: boolean;
+    charisma?: boolean;
+}
+
+export interface ISkills {
+    //18 total skills, 0 is none, 1 is proficient, 2 is expertise
+    //str
+    athletics?: number;
+    //dex
+    acrobatics?: number;
+    sleightOfHand?: number;
+    stealth?: number;
+    //int
+    arcana?: number;
+    history?: number;
+    investigation?: number;
+    nature?: number;
+    religion?: number;
+    //wis
+    animalHandling?: number;
+    insight?: number;
+    medicine?: number;
+    perception?: number;
+    survival?: number;
+    //cha
+    deception?: number;
+    intimidation?: number;
+    performance?: number;
+    persuasion?: number;
+}
+
+export interface IDamageResistImmune {//1 is resist, 2 is immune
+    acid?: number;
+    bludgeoning?: number;
+    cold?: number;
+    fire?: number;
+    force?: number;
+    lightning?: number;
+    necrotic?: number;
+    piercing?: number;
+    poison?: number;
+    psychic?: number;
+    radiant?: number;
+    slashing?: number;
+    thunder?: number;
+}
+
+export interface IDamageVulnerable {
+    acid?: boolean;
+    bludgeoning?: boolean;
+    cold?: boolean;
+    fire?: boolean;
+    force?: boolean;
+    lightning?: boolean;
+    necrotic?: boolean;
+    piercing?: boolean;
+    poison?: boolean;
+    psychic?: boolean;
+    radiant?: boolean;
+    slashing?: boolean;
+    thunder?: boolean;
+}
+
+export interface IConditionImmune {
+    blinded?: boolean;
+    charmed?: boolean;
+    deafened?: boolean;
+    exhaustion?: boolean;
+    frightened?: boolean;
+    grappled?: boolean;
+    incapacitated?: boolean;
+    invisible?: boolean;
+    paralyzed?: boolean;
+    petrified?: boolean;
+    poisoned?: boolean;
+    prone?: boolean;
+    restrained?: boolean;
+    stunned?: boolean;
+    unconscious?: boolean;
+}
+
+export interface ISenses {
+    blindsight?: number;
+    darkvision?: number;
+    tremorsense?: number;
+    truesight?: number;
+    //passive
+}
 
 export default Creature;
