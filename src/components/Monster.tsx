a649a968ed6c67f7519009c4d3decad71f7e0ca6
Matthew Evangelidis <matthew.evangelidis@students.mq.edu.au>
2021-01-02 18:55:11 +1100

    Monster component now uses the Creature class in place of props and now destructures its fields

--- src/components/Monster.tsx
+++ src/components/Monster.tsx
@@ -11,33 +11,56 @@ import { Senses } from "./Senses";
 import { Languages } from "./Languages";
 import { Traits } from "./Traits";
 import { Actions } from "./Actions";
+import { Reactions } from "./Reactions";
+import { LegendaryActions } from "./LegendaryActions";
 
-interface Props {
-    creature: Creature;
-};
+const Monster: React.FC<Creature> = ({ 
+    name,
+    size,
+    type,
+    alignment,
+    proficiency,
+    hitPoints,
+    armorClass,
+    speeds,
+    stats,
+    saves,
+    skills,
 
-const Monster: React.FC<Props> = ({ creature }) => {
+    damageVulnResistImmune,
+    conditionImmunes,
+    senses,
+
+    languages,
+    challenge,
+    traits,
+    actions,
+    reactions,
+    legendary,
+    legendaryActions,
+    mythic
+}) => {
 
     return(
         <div>
-            <h1>{creature.name}</h1>
-            <h2>{creature.size} {creature.type}, {creature.alignment}</h2>
-            <h2>Armor Class {creature.armorClass.value} ({creature.armorClass.from})</h2>
-            <h2>Hit Points {creature.hitPoints.value} {creature.hitPoints.formula}</h2>
+            <h1>{name}</h1>
+            <h2>{size} {type}, {alignment}</h2>
+            <h2>Armor Class {armorClass.value} ({armorClass.from})</h2>
+            <h2>Hit Points {hitPoints.value} {hitPoints.formula}</h2>
 
-            <Speed speeds={creature.speeds}/>
-            <Stats stats={creature.stats}/>
-            <SavingThrows saves={creature.saves}stats={creature.stats} proficiency ={creature.proficiency}/>
-            <Skills skills={creature.skills} stats={creature.stats} proficiency={creature.proficiency}/>
-            <DamageVulnResistImmune damages={creature.damageVulnResistImmune}/>
-            <ConditionImmunes conditions={creature.conditionImmunes}/>
-            <Senses senses={creature.senses} stats={creature.stats} skills={creature.skills} proficiency={creature.proficiency}/>
-            <Languages languages={creature.languages}/>
-            <p>Challenge {creature.challenge}</p>
-            <Traits traits={creature.traits}/>
-            <Actions attacks={creature.actions?.attacks} effects={creature.actions?.effects} multiattack={creature.actions?.multiattack}/>
-            {/* Reactions */}
-            {/* Legendary Actions */}
+            <Speed speeds={speeds}/>
+            <Stats stats={stats}/>
+            <SavingThrows saves={saves}stats={stats} proficiency ={proficiency}/>
+            <Skills skills={skills} stats={stats} proficiency={proficiency}/>
+            <DamageVulnResistImmune damages={damageVulnResistImmune}/>
+            <ConditionImmunes conditions={conditionImmunes}/>
+            <Senses senses={senses} stats={stats} skills={skills} proficiency={proficiency}/>
+            <Languages languages={languages}/>
+            <p>Challenge {challenge}</p>
+            <Traits traits={traits}/>
+            <Actions attacks={actions?.attacks} effects={actions?.effects} multiattack={actions?.multiattack}/>
+            <Reactions reactions={reactions}/>
+            {legendaryActions && <LegendaryActions desc={legendaryActions.desc} actions={legendaryActions.actions}/>}
             
         </div>
     );
